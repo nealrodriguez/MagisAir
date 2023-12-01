6ab3ca5449d87357312d71e13fab0beb00651f15
NealRodriguez <neal.rodriguez@obf.ateneo.edu>
2023-12-01 16:20:01 +0800

    MySQL migrated and Django View working

--- MagisAir/MagisAir/settings.py
+++ MagisAir/MagisAir/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'flights',
 ]
 
 MIDDLEWARE = [
@@ -78,8 +79,11 @@ WSGI_APPLICATION = 'MagisAir.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PORT': '3307',
     }
 }
 
